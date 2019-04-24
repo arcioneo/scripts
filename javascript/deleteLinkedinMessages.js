b2a2c55a45716f4b8844acd87a78414c1acf20cb
Eduardo Milpas <eduardomilpas@gmail.com>
2019-04-23 22:14:25 -0400

    Update deleteLinkedinMessages.js

--- javascript/deleteLinkedinMessages.js
+++ javascript/deleteLinkedinMessages.js
@@ -37,3 +37,23 @@ const timeout = 1000;
 while	($(".msg-conversation-listitem__link:eq(0)").length > 0) {
 	await executeAfterTimeout(clickFirstMessage, timeout).then(executeAfterTimeout.bind(this, clickOverFlowButton, timeout)).then(executeAfterTimeout.bind(this, clickDeleteConversationButton, timeout)).then(executeAfterTimeout.bind(this, clickDeleteConfirmButton, timeout));
 }
+
+
+========ALTERNATIVE SOLUTION(it worked on apr 23 2019)=========
+	;(function () {
+  setInterval(() => {
+    let deleted = false;
+    $('artdeco-dropdown-item').each((i, elem) => {
+      let txt = $(elem).text().trim();
+      //if (txt === 'Delete') {
+        deleted = true;
+        $(elem).click();
+      //}
+    });
+    if (deleted){
+      setTimeout(() => $('.js-msg-delete').click(), 500)
+    } else {
+      $('.msg-conversation-card__list-action-icon').eq(1).click();
+    }
+  }, 1000);
+}());
