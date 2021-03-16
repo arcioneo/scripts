12354161f9eec450091d4d43e69f782d576e5b4b
Eduardo Milpas <eduardomilpas@gmail.com>
2021-03-15 18:49:46 -0600

    Update tomp4.sh

--- bash-scripts/tomp4.sh
+++ bash-scripts/tomp4.sh
@@ -2,16 +2,29 @@
 
 usage() {
     cat << EOF  
-Usage: Converts a webm file to mp4 on the same path(or all webm files in a folder).
+Usage: Converts a webm/avi file to mp4 on the same path(or all webm files in a folder).
 	tomp4 -f <path-to-file or folder>
 EOF
 }
 
  convertToMp4() {
  	filename=$1
-	newFileName="${filename/.webm/.mp4}"
-	echo "converting file $filename"
-	ffmpeg -hide_banner -loglevel error -i $filename -c:v libx264 -c:a aac -strict experimental -b:a 192k $newFileName
+ 	echo "converting file $filename"
+ 	if [[ "$filename" == *.avi ]]
+ 	then
+		newFileName="${filename/.avi/.mp4}"
+		ffmpeg -hide_banner -loglevel error -i "$filename" "$newFileName"
+	elif [[ "$filename" == *.webm ]]
+	then
+		newFileName="${filename/.webm/.mp4}"
+		ffmpeg -hide_banner -loglevel error -i "$filename" -c:v libx264 -c:a aac -strict experimental -b:a 192k "$newFileName"
+	elif [[ "$filename" == *.mkv ]]
+	then
+		newFileName="${filename/.mkv/.mp4}"
+		ffmpeg -hide_banner -loglevel error -i "$filename" "$newFileName"
+	else
+		echo "File $filename won't be converted"
+	fi
 }
 
 
@@ -38,7 +51,8 @@ if [[ -d $FOLDER ]]; then
 		FOLDER=${FOLDER%?}
 	fi
     echo "Converting files in directory"
-    for filename in $FOLDER/*.webm; do
+    for filename in $FOLDER/*.*
+    do
         convertToMp4 "${filename}"
     done
 elif [[ -f $FOLDER ]]; then
